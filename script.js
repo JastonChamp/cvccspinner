4018f4e1fb0e4831eab17f681f9e7b16039bbb45
JastonChamp <142294191+JastonChamp@users.noreply.github.com>
2023-09-27 14:20:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
 const wheel = document.querySelector('.wheel');
-const cvccWords = [
-   'bend', 'milk', 'jump', 'sink', 'wind', 'lost', 'sand', 'melt',
-  'lamp', 'belt', 'gulp', 'band', 'limp', 'pink', 'wing',
-  'pond', 'rust', 'vest',  'bolt',  'fist', 'golf',
-  'hunt', 'junk', 'kept', 'lend', 'musk', 'nest', 'pint', 'ramp', 
-  'tent', 'wilt',  'zest', 'bulk', 'chip', 'dock', 'funk', 
-  'hulk', 'inch', 'jolt',  'lump', 'mint', 'neck',  'pump',
-  'rack', ' 'tick', 'volt',  'yolk', 
-    'itch', 'jazz',  'lock', 
-  'numb', 'pack', 'quit', 'risk', 'smog', 'tack', 'vent',  
-    'dust', 'film', 'grew', 'hiss',  'kill',
-    'swim',  
-  'bump', ' 'dung',   'huff',
-  'kilt', 'lint', 'muck', 'puck', 'rung', 'shut', 'tusk', 'wisp'
+const ccvcWords = [
+  'brag', 'clam', 'drop', 'flag', 'grip', 
+  'plan', 'slip', 'trap', 'blot', 'clap',
+  'drum', 'flap', 'glum', 'plug',
+  'skim', 'twin',  'brim', 'clot', 'drip',
+  'frog', 'swim', 
+   'blip', 'chip',  'twig',
+  'blab', 'clod', 'flit', 'grit',  'plot', 'scam',
+  'trim', 'bran', 'chop', 'drag', 'flop', 'glim', 
+  'prod', 'slam', 'twit', 
+  'swan', 'sled', 'swig', 'slug',  'slab', 'swop', 'slop', 'swat',
+  'swot', 'brat', 'clad',  'flog', 'grub', 
+   'plum',  'spit', 'trot',  'bloc', 
+  'plop', 
+  'spin', 
+'scum', 
+  'blow', 'claw',  'grit', 
+  'plug',  'skim', 'twin', 
 ];
 
-cvccWords.forEach((word, index) => {
+ccvcWords.forEach((word, index) => {
   const slot = document.createElement('div');
   slot.className = 'slot';
-  
-  let coloredWord = '';
-  for (let letter of word) {
-    if ('aeiou'.includes(letter)) {
-      coloredWord += `<span class="vowel">${letter}</span>`;
-    } else {
-      coloredWord += letter;
-    }
-  }
-  
-  slot.innerHTML = coloredWord;
+  slot.textContent = word; // Display the CCVC word
   slot.style.display = 'none';
   wheel.appendChild(slot);
 });
@@ -44,7 +38,7 @@ document.getElementById('spinButton').addEventListener('click', () => {
 
   const shuffleEffect = setInterval(() => {
     slots[lastRandom].style.display = 'none';
-    const randomSlot = Math.floor(Math.random() * cvccWords.length);
+    const randomSlot = Math.floor(Math.random() * ccvcWords.length);
     slots[randomSlot].style.display = 'flex';
     lastRandom = randomSlot;
     shuffleCount++;
@@ -56,7 +50,7 @@ document.getElementById('spinButton').addEventListener('click', () => {
   }, 100);
 
   setTimeout(() => {
-    const randomSlot = Math.floor(Math.random() * cvccWords.length);
+    const randomSlot = Math.floor(Math.random() * ccvcWords.length);
     slots[currentSlot].style.display = 'none';
     slots[randomSlot].style.display = 'flex';
     currentSlot = randomSlot;
